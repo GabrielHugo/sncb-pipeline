a14a1c6e690d2ac5d4955b82f63ca730028a972e
GabrielHugo <hugogabriel1412@hotmail.com>
2026-09-22 21:24:54 +0200

    ingestion to bronze

--- ingestion/main.py
+++ ingestion/main.py
@@ -1,4 +1,11 @@
 import requests
+from datetime import datetime
+
+f_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+bronze_file = f"data/bronze/namur_{f_time}.json"
 
 x = requests.get('https://api.irail.be/v1/liveboard?station=Namur&format=json&lang=fr')
-print(x.json())
+
+with open(bronze_file, "w", encoding="utf-8") as f:
+    f.write(x.text)
