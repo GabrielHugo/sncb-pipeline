2e31df39371cb233397d8aa18713bd635fcad300
GabrielHugo <hugogabriel1412@hotmail.com>
2026-09-23 14:21:56 +0200

    add multi-station ingestion with error handling

--- ingestion/main.py
+++ ingestion/main.py
@@ -1,11 +1,34 @@
 import requests
 from datetime import datetime
+import time
 
-f_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+stations = ["Namur", "Gent-Sint-Pieters", "Luxembourg", "Hugo", "Liège", "Anvers"]
 
-bronze_file = f"data/bronze/namur_{f_time}.json"
+for station in stations :
 
-x = requests.get('https://api.irail.be/v1/liveboard?station=Namur&format=json&lang=fr')
+    f_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-with open(bronze_file, "w", encoding="utf-8") as f:
-    f.write(x.text)
+    bronze_file = f"data/bronze/{station}_{f_time}.json"
+
+    try:
+
+        rr = requests.get(f'https://api.irail.be/v1/liveboard?station={station}&format=json&lang=fr')
+
+        status = rr.status_code
+
+        if status == 200:
+
+            print(f"Status {status} is ok!")
+
+            with open(bronze_file, "w", encoding="utf-8") as f:
+                f.write(rr.text)
+
+            
+
+        else:
+              print(f"Status {status} isn't ok!")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error for {station} : {e}")
+
+    time.sleep(0.5)
