847359e42fb66bb8df2b9c8677ff179b0d36ca45
GabrielHugo <hugogabriel1412@hotmail.com>
2026-09-24 00:38:15 +0200

    add success and failure counters

--- ingestion/main.py
+++ ingestion/main.py
@@ -4,6 +4,9 @@ import time
 
 stations = ["Namur", "Gent-Sint-Pieters", "Luxembourg", "Hugo", "Liège", "Anvers"]
 
+v_counter = 0
+f_counter = 0
+
 for station in stations :
 
     f_time = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -21,10 +24,17 @@ for station in stations :
             with open(bronze_file, "w", encoding="utf-8") as f:
                 f.write(rr.text)
 
+            v_counter+=1
+
         else:
               print(f"Failed for {station}: {status}")
+              f_counter+=1
 
     except requests.exceptions.RequestException as e:
         print(f"Error for {station} : {e}")
+        f_counter+=1
 
     time.sleep(0.5)
+
+print(f"{v_counter} victory")
+print(f"{f_counter} failure")
